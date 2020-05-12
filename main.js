0a7ec6411e484b4531d3e3c46ea710509c371bae
Sarah Choung <sarahjchoung@gmail.com>
2020-05-12 11:29:57 -0700

    added basic YT video functionality

--- main.js
+++ main.js
@@ -1,32 +1,49 @@
 // NYTimes  API key: HaKYObfArvAiEEXXYE4S7EXBYOd0mpP0
 // YT API key: AIzaSyDk49iU_ZTZcnVKaF3dRbOtFEtZWhINHgc
 
-// $.ajax({
-//   url: "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=HaKYObfArvAiEEXXYE4S7EXBYOd0mpP0",
-//   success: logResult,
-//   error: logError
-// })
 
-// function logResult(result) {
-//   console.log("success!", result)
-// }
+//YOUTUBE VIDEO
+function getVideo() {
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: {
+      key: 'AIzaSyDk49iU_ZTZcnVKaF3dRbOtFEtZWhINHgc',
+      q: "videogamedunkey",
+      part: 'snippet',
+      maxResults: 1,
+      type: 'video',
+      videoEmbeddable: true,
+    },
+    success: function (data) {
+      embedVideo(data)
+    },
+    error: function (response) {
+      console.log("Request Failed");
+    }
+  });
+}
 
-// function logError(err) {
-//   console.log("error", err)
-// }
+function embedVideo(data) {
+  $('iframe').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId)
+  $('h4.title').text(data.items[0].snippet.title)
+  $('.description').text(data.items[0].snippet.description)
+}
 
 
-// $.ajax({
-//   type: 'GET',
-//   url: 'https://www.googleapis.com/youtube/v3/search',
-//   data: {
-//     key: 'AIzaSyDk49iU_ZTZcnVKaF3dRbOtFEtZWhINHgc',
-//     q: "godfather",
-//     part: 'snippet',
-//     maxResults: 1,
-//     type: 'video',
-//     videoEmbeddable: true,
-//   },
-//   success: logResult,
-//   error: logError
-// })
+//NY TIMES ARTICLE
+function getArticle() {
+  $.ajax({
+    url: "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=HaKYObfArvAiEEXXYE4S7EXBYOd0mpP0",
+    success: logResult,
+    error: logError
+  })
+
+  function logResult(result) {
+    console.log("success!", result)
+  }
+
+  function logError(err) {
+    console.log("error", err)
+  }
+}
