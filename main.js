09e6bced40e70cc3fdc6e6fcf1ca8548231f8072
Sarah Choung <sarahjchoung@gmail.com>
2020-05-12 14:58:25 -0700

    form, nytimes, youtube functionalities work

--- main.js
+++ main.js
@@ -2,21 +2,35 @@
 // YT API key: AIzaSyDk49iU_ZTZcnVKaF3dRbOtFEtZWhINHgc
 
 
+//Form Submission
+var userInput;
+var form = document.querySelector("form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  var textInput = event.target[0];
+  var textValue = textInput.value;
+  console.log(textValue);
+  var textNoSpace = textValue.split(" ").join("_");
+  console.log(textNoSpace);
+  userInput = textNoSpace;
+})
+
+
 //YOUTUBE VIDEO
 function getVideo() {
   $.ajax({
     type: 'GET',
     url: 'https://www.googleapis.com/youtube/v3/search',
     data: {
-      key: 'AIzaSyDk49iU_ZTZcnVKaF3dRbOtFEtZWhINHgc',
-      q: "videogamedunkey",
+      key: youtubeAPIKey,
+      q: userInput,
       part: 'snippet',
       maxResults: 1,
       type: 'video',
       videoEmbeddable: true,
     },
-    success: function (data) {
-      embedVideo(data)
+    success: function(data) {
+      embedVideo(data);
     },
     error: function (response) {
       console.log("Request Failed");
@@ -34,13 +48,21 @@ function embedVideo(data) {
 //NY TIMES ARTICLE
 function getArticle() {
   $.ajax({
-    url: "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=HaKYObfArvAiEEXXYE4S7EXBYOd0mpP0",
-    success: logResult,
+    url: "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=" + userInput + "&api-key=" + nyTimesAPIKey,
+    success: embedArticle,
     error: logError
   })
 
-  function logResult(result) {
-    console.log("success!", result)
+  function embedArticle(data) {
+    console.log(data.results);
+    var articleResults = data.results;
+    console.log(articleResults[0].link);
+    var articleLink = articleResults[0].link;
+    console.log(articleLink.url);
+    var articleURL = articleLink.url;
+    var nyTimeLink = document.createElement("p");
+    nyTimeLink.append(articleURL);
+    document.querySelector("div#nydiv").append(nyTimeLink);
   }
 
   function logError(err) {
