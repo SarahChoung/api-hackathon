7a1e242316d2848cc1d70e708aea549c7ce8f559
Sarah Choung <sarahjchoung@gmail.com>
2020-05-12 10:34:07 -0700

    Search Bar and Submit/Clear buttons added

---
+++ main.js
@@ -0,0 +1,32 @@
+// NYTimes  API key: HaKYObfArvAiEEXXYE4S7EXBYOd0mpP0
+// YT API key: AIzaSyDk49iU_ZTZcnVKaF3dRbOtFEtZWhINHgc
+
+// $.ajax({
+//   url: "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=HaKYObfArvAiEEXXYE4S7EXBYOd0mpP0",
+//   success: logResult,
+//   error: logError
+// })
+
+// function logResult(result) {
+//   console.log("success!", result)
+// }
+
+// function logError(err) {
+//   console.log("error", err)
+// }
+
+
+// $.ajax({
+//   type: 'GET',
+//   url: 'https://www.googleapis.com/youtube/v3/search',
+//   data: {
+//     key: 'AIzaSyDk49iU_ZTZcnVKaF3dRbOtFEtZWhINHgc',
+//     q: "godfather",
+//     part: 'snippet',
+//     maxResults: 1,
+//     type: 'video',
+//     videoEmbeddable: true,
+//   },
+//   success: logResult,
+//   error: logError
+// })
